e9524b713e90a89d9b97194e00eb85e08749ad0f
Oumuamua <aryanandroid26@gmail.com>
2025-04-15 12:26:55 +0530

    added Prometheus client

--- bash.sh
+++ bash.sh
@@ -2,5 +2,6 @@
 python -m venv .venv
 source .venv/bin/activate
 pip install -r ./requirements.txt
+pip install prometheus-fastapi-instrumentator
 uvicorn main:app --host 0.0.0.0 --port 80 --reload
 
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI, Form, Request
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
-
+from prometheus_fastapi_instrumentator import Instrumentator
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 todos = []
-
+Instrumentator().instrument(app).expose(app)
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request, "todos": todos})
